3e9351b56413783b55e6e7876de410b8819f5efd
ydvmudit07 <ydvmudit07@gmail.com>
2023-11-03 16:02:49 +0530

    setting up the pages directory

---
+++ pages/product/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Product Home</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h1{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    font-family: cursive;
+}
+</style>
