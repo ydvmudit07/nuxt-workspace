c7c1499465af7684fd5010fe360b1a2e251d56e4
ydvmudit07 <ydvmudit07@gmail.com>
2023-11-03 20:32:29 +0530

    setting up api and dynamic-routing (api)

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,11 +1,19 @@
 <template>
     <div>
         <h1>Product About {{ id }}</h1>
+        <p>{{ product.title }}</p>
+        <p>{{ product.price }}</p>
+        <p>{{ product.id }}</p>
     </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params
+const uri = "https://fakestoreapi.com/products/" + id
+
+const { data: product } = await useFetch(uri,{
+    key: id
+})
 </script>
 
 <style scoped>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
         <h1>Product Home</h1>
+        <div v-for="product in product">
+            <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+        </div>
     </div>
 </template>
 
 <script setup>
+
+const uri = "https://fakestoreapi.com/products"
+const { data: product } = await useFetch(uri)
+
 definePageMeta({
     layout: "products",     
 })
